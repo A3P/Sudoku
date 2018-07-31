2bcce1c733953e967d2f5a222f589aa375ee60ce
n6000 <41807569+n6000@users.noreply.github.com>
2018-07-30 18:25:24 -0700

    Add files via upload

--- sud2sat.py
+++ sud2sat.py
@@ -30,10 +30,10 @@ def parsePuzzle(input_file):
 def base9To10(i,j,k):
     return 81*i + 9*j + k + 1
 
-#Creates clauses for the cells set by the input file
+#Clauses for the preset cells set by the input file
 def genInitialVars(puzzle):
     global n
-    string = "";
+    string = "(Initial Clauses)\n";
     for i in range(9):
         for j in range(9):
             if puzzle[i][j] != '0':
@@ -41,17 +41,47 @@ def genInitialVars(puzzle):
                 n += 1
     return string
 
+#Clauses ensuring each cell has a number
+def genCellVars():
+    global n
+    string = "(Cell Clauses)\n"
+    for i in range(9):
+        for j in range(9):
+            for k in range(9):
+                string += str(base9To10(i,j,k)) + ' '
+            string  += "0\n"
+            n += 1
+    return string
+
+#Clauses ensuring uniqueness through rows
+def genRowVars():
+    global n
+    string = "(Row Clauses)\n"
+
+    return string
+
+#Clauses ensuring uniqueness through columns
+def genColVars():
+    global n
+    string = "(Column Clauses)\n"
+
+    return string
+
+#Clauses ensuring uniqueness through 3x3 sub-grids
+def genSubGridVars():
+    global n
+    string = "(Sub-Grid Clauses)\n"
+
+    return string
 
 def main():
-
     if len(sys.argv) < 1:
         print("Provide an input file")
         sys.exit(-1)
 
     puzzle = parsePuzzle(sys.argv[1])
 
-    dimacs_string = genInitialVars(puzzle)
-    #dimacs_string += ... add more clauses
+    dimacs_string = genInitialVars(puzzle) + genCellVars() + genRowVars() + genColVars() + genSubGridVars()
 
     print("p cnf 729 " + str(n) + "\n" + dimacs_string)
 
