c66308da2d5c0dd4ff91fc3fa4a86fb3a2e053a3
dmwcq <41886934+dmwcq@users.noreply.github.com>
2018-07-31 23:24:28 -0700

    Update sat2sud.py

--- sat2sud.py
+++ sat2sud.py
@@ -1,3 +1,5 @@
+#sat2sud reads the output produced by miniSAT for a given puzzle instance and converts it back into
+#a solved Sudoku puzzle (suitable for printing)
 import sys
 import os
 def genSudoku(s):
