d60be7ee67160814ff24bc63c82a6d0b02093b68
Ryan Afshar <ryanafsharryan@gmail.com>
2018-08-05 08:01:55 -0700

    Update script for Task 1 generation

--- task.sh
+++ task.sh
@@ -2,6 +2,7 @@
 
 rm timeList.txt
 rm basicTaskOut.txt
+rm task1Output.txt
 
 line=2
 for grid in {1..50}
@@ -17,3 +18,17 @@ do
     line=$((line+10))
 done
 python3 avgtime.py < timeList.txt >> basicTaskOut.txt
+
+rm timeList.txt
+for grid in {1..95}
+do
+    echo "Grid $((grid))" >> task1Output.txt
+    sed -n $((grid))p task1Input.txt > grid.txt
+    python3 sud2sat.py grid.txt > sat.txt
+    minisat sat.txt solution.txt | grep CPU > time.txt
+    python3 sat2sud.py solution.txt >> task1Output.txt
+    cat time.txt >> task1Output.txt
+    echo $'\n' >> task1Output.txt
+    cat time.txt >> timeList.txt
+done
+python3 avgtime.py < timeList.txt >> task1Output.txt
